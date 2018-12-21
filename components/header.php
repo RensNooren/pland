d814bacd86e305b33eabb18975fca5dc785c1399
Rens Nooren <rensnooren@gmail.com>
2018-12-21 14:47:02 +0100

    added components

---
+++ components/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'db_connection.php';?>
+<!--    <div class="se-pre-con">-->
+<!--        <img src="media/loading_icon.gif" class="loading_icon" alt="">-->
+<!--    </div>-->
+    <meta charset="UTF-8">
+    <title>Elementary Homework Assistent</title>
+
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="navigation.js"></script>
+    <script src="javascript.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
